aafbf4f66f3926f878cc4d3b926dd6f5a2b4821b
PyGeek03 <PyGeek03@users.noreply.github.com>
2016-11-06 01:07:17 +0700

    Use /tmp to hold temporary data

--- start.sh
+++ start.sh
@@ -1,6 +1,23 @@
-#!/bin/sh
+#!/bin/bash
+mkdir /tmp/starter-conky
 
-conky -c co_main > log &
-conky -c co_weather > log &
-conky -c co_fact > log &
-conky -c co_quote > log &
+function update
+{
+    TMP_FILE="$1"
+    CACHE_FILE="$2"
+    if [ -e $TMP_FILE ]; then
+        cp $TMP_FILE $CACHE_FILE
+    else
+        cp $CACHE_FILE $TMP_FILE
+    fi
+}
+
+update /tmp/starter-conky/fact.tmp    Cache/fact.tmp
+update /tmp/starter-conky/quote.tmp   Cache/quote.tmp
+update /tmp/starter-conky/weather.tmp Cache/weather.tmp
+
+sleep 5
+conky -d -c co_main    > /tmp/starter-conky/starter-conky.log &
+conky -d -c co_weather > /tmp/starter-conky/starter-conky.log &
+conky -d -c co_fact    > /tmp/starter-conky/starter-conky.log &
+conky -d -c co_quote   > /tmp/starter-conky/starter-conky.log &
